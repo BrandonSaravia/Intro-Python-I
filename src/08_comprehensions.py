468f86b7de2ec25777b0c14d321ce6ab0cfb76a5
BrandonSaravia <BPSaravia@gmail.com>
2019-08-05 18:36:08 -0400

    finished all questions on files 0, 1, 2, 4, 5, 7, 8  currently on file 6

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -12,12 +12,16 @@ for more info regarding list comprehensions.
 
 y = []
 
+for x in range(5):
+    x +=1
+    y.append(x)
+
 print (y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+y = [x**3 for x in range(10)]
 
 print(y)
 
@@ -26,7 +30,7 @@ print(y)
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [x.upper() for x in a]
 
 print(y)
 
